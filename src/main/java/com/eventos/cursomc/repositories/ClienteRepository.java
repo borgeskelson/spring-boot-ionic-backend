3966ead7ef47f27e507d242c0260fefdcacef320
Kelson Borges <borgeskelson@gmail.com>
2018-02-15 17:20:34 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/eventos/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.eventos.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eventos.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+}
---
+++ src/main/java/com/eventos/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.eventos.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eventos.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
+
+}

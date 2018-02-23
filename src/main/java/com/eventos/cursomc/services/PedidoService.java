78079d39d22ed177316138fcecdf55bbe7cfffa0
Kelson Borges <borgeskelson@gmail.com>
2018-02-23 11:55:05 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/eventos/cursomc/services/PedidoService.java
+++ src/main/java/com/eventos/cursomc/services/PedidoService.java
@@ -3,16 +3,23 @@ package com.eventos.cursomc.services;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.eventos.cursomc.domain.Cliente;
 import com.eventos.cursomc.domain.ItemPedido;
 import com.eventos.cursomc.domain.PagamentoBoleto;
 import com.eventos.cursomc.domain.Pedido;
 import com.eventos.cursomc.domain.enums.EstadoPagamento;
+import com.eventos.cursomc.repositories.ClienteRepository;
 import com.eventos.cursomc.repositories.ItemPedidoRepository;
 import com.eventos.cursomc.repositories.PagamentoRepository;
 import com.eventos.cursomc.repositories.PedidoRepository;
 import com.eventos.cursomc.repositories.ProdutoRepository;
+import com.eventos.cursomc.security.UserSS;
+import com.eventos.cursomc.services.exceptions.AuthorizationException;
 import com.eventos.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,6 +27,9 @@ public class PedidoService {
 	
 	@Autowired
 	private PedidoRepository repo;
+	
+	@Autowired
+	private BoletoService boletoService;
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
@@ -31,7 +41,7 @@ public class PedidoService {
 	private ItemPedidoRepository itemPedidoRepository;
 	
 	@Autowired
-	private BoletoService boletoService;
+	private ClienteRepository clienteRepository;
 	
 	public Pedido find(Integer id) {
 		Pedido obj = repo.findOne(id);
@@ -64,6 +74,18 @@ public class PedidoService {
 		itemPedidoRepository.save(obj.getItens());
 		
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteRepository.findOne(user.getId());
+		
+		return repo.findByCliente(cliente, pageRequest);
 	}
 
 }
